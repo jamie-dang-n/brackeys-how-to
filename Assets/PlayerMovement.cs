801c6e6ee6e0daf899fcbf4fb36c7997d4d513cf
gothhouse <jamiedang1349@gmail.com>
2019-11-18 09:42:14 -0800

    Finished E03. Added input for player mvt from keyboard and physics material so player doesn't fly off

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,7 +4,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
-
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
 
     void Start()
     {
@@ -13,6 +14,16 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
