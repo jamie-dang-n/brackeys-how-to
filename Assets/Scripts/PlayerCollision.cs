6b14803e1110228f10ddb55775bf06b3d72f530a
gothhouse <jamiedang1349@gmail.com>
2019-11-26 09:43:27 -0800

    Added GameOver restart

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -9,7 +9,8 @@ public class PlayerCollision : MonoBehaviour
     {
         if (collisionInfo.collider.tag == "Obstacle")
         {
-            movement.enabled = false;   
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
