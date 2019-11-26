5292f8c61533be8482d6b652a6210e0f2e06cdc2
gothhouse <jamiedang1349@gmail.com>
2019-11-26 10:01:58 -0800

    Added GameWin window, but still need to animate it

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,13 @@ public class GameManager : MonoBehaviour
 
     public float restartDelay = 1f;
 
+    public GameObject completeLevelUI;
+
+    public void WinGame ()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
     public void EndGame ()
     {
         if (gameHasEnded == false)
